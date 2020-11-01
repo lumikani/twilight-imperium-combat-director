32e0b9ffad0ef73980ca62fe8251147b2b9055d6
Lumi <kallioniemi@pm.me>
2020-11-01 14:00:31 +0200

    Improvements to app state management

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,36 @@
-import { Question } from './types/appTypes'
+import { Question, PromptType } from './types/appTypes'
 
 export const log = (data: any) => {
   console.log(JSON.stringify(data, null, 2))
 }
 
-export const createQuestion = (name: string, question: string): Question => ({
-  type: 'input',
-  name,
-  message: question,
-})
+const createInquirerQuestion = (
+  type: PromptType,
+  name: string,
+  question: string,
+  defaultValue: string | number | boolean | undefined
+): Question => {
+  const questionObject: Question = {
+    type,
+    name,
+    message: question,
+  }
+
+  if (defaultValue !== undefined) {
+    questionObject.default = defaultValue
+  }
+
+  return questionObject
+}
+
+export const createQuestion = (
+  name: string,
+  question: string,
+  defaultValue?: string | number
+): Question => createInquirerQuestion('input', name, question, defaultValue)
+
+export const createBooleanChoice = (
+  name: string,
+  question: string,
+  defaultValue?: boolean
+): Question => createInquirerQuestion('confirm', name, question, defaultValue)
